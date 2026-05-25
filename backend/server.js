0295e6c422d98a2717de0938170c767580c5d6ed
Tim <timote.godard@gmail.com>
2026-05-25 15:19:18 +0200

    changement de variables

--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,4 @@
-require('dotenv').config();
+require('dotenv').config({ override: true });
 const express = require('express');
 const cors = require('cors');
 const mysql = require('mysql2/promise'); // On utilise la version 'promise' pour un code plus moderne
