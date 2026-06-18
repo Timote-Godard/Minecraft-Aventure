4490b3844fcbe6ae5f8522ff04517eee145b0e11
Timoté Godard <timote.godard@gmail.com>
2026-06-18 13:52:21 +0200

    modification du filtre

--- frontend/src/Boutique/shopUtils.ts
+++ frontend/src/Boutique/shopUtils.ts
@@ -5,13 +5,15 @@ const NO_MATERIALS_ITEMS = ['elytra', 'bow', 'crossbow', 'shield', 'trident','ca
 export const filterMinecraftEquipment = (item: ShopItem, activeLeftId: string, activeRightId?: string) => {
   if (!item.target_item) return false;
 
-  if (NO_MATERIALS_ITEMS.includes(item.target_item)) {
-    return activeLeftId === item.target_item;
+  const cleanTarget = item.target_item.replace('minecraft:', '');
+
+  if (NO_MATERIALS_ITEMS.includes(cleanTarget)) {
+    return activeLeftId === cleanTarget;
   }
 
   if (!activeRightId) return false;
 
-  const [itemMaterial, itemCategory] = item.target_item.split('_');
+  const [itemMaterial, itemCategory] = cleanTarget.split('_');
   return itemMaterial === activeRightId && itemCategory === activeLeftId;
 };
 
